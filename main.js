78b7bc9efa5ae9a94f2ba754d6570ee053f7d460
ashritaravishankar <77598737+ashritaravishankar@users.noreply.github.com>
2021-04-07 23:23:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first ="My first console project";
+console.log(first);
+
+var name_first ="Ashrita";
+var name_sur ="Ravishankar";
+var full_name =name_first.concat(name_sur);
+console.log(full_name);
+var num_first ="4";
+var num_sec ="5";
+var add =(num_first+num_sec);
+console.log(add);
